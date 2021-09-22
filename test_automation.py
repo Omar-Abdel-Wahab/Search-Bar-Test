c04e1cdac239aeb2e775d4842b3b5fa546d472b6
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2021-09-22 18:33:09 +0200

    Added unit testing for the automation script

--- test_automation.py
+++ test_automation.py
@@ -1,24 +1,24 @@
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import search_test
+import urllib.parse
 
 
 class ChromeSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome('./chromedriver')
+        self.google_bar = search_test.SearchTest("testcases.txt", "./chromedriver.exe", "https://www.google.com")
+        self.google_bar.driver_setup()
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("https://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("getting started with python")
-        elem.send_keys(Keys.RETURN)
-        assert "https://www.python.org/search/?q=getting+started+with+python&submit=" == driver.current_url
+    def test_google_search_bar(self):
+        self.google_bar.extract_testcases()
+        self.google_bar.get_site()
+        for search_bar_testcase in self.google_bar.testcases:
+            self.google_bar.test_single_testcase(search_bar_testcase)
+            self.assertIn(f"https://www.google.com/search?q={urllib.parse.quote_plus(search_bar_testcase)}",
+                          self.google_bar.driver.current_url)
 
     def tearDown(self):
-        self.driver.close()
+        self.google_bar.driver.close()
 
 
 if __name__ == "__main__":
