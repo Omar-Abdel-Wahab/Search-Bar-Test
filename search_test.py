cf7395bcbd36b0ab99f1ff16dff987c984002efe
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2021-09-22 13:52:04 +0200

    Structured code in OOP + added exception handling

--- search_test.py
+++ search_test.py
@@ -2,30 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-def is_test_case(line):
-    if line.startswith("\n") or line.startswith("#"):
-        return False
-    return True
+class SearchTest:
+    def __init__(self, file_path, driver_path, site):
+        self.file_path = file_path
+        self.driver_path = driver_path
+        self.site = site
+
+    @staticmethod
+    def is_test_case(line):
+        if line.startswith("\n") or line.startswith("#"):
+            return False
+        return True
+
+    def driver_setup(self):
+        try:
+            driver = webdriver.Chrome(self.driver_path)
+            driver.get(self.site)
+        except Exception as e:
+            print(e)
+        else:
+            return driver
+
+    def extract_test_cases(self):
+        try:
+            with open(self.file_path, encoding="utf-8") as file:
+                lines = file.readlines()
+                test_cases = [line.rstrip() for line in lines if self.is_test_case(line)]
+                test_cases = [test_case if test_case != "Null" else "" for test_case in test_cases]
+        except Exception as e:
+            print(e)
+        else:
+            return test_cases
+
+    def test(self):
+        driver = self.driver_setup()
+        test_cases = self.extract_test_cases()
+        try:
+            for test_case in test_cases:
+                search_bar = driver.find_element_by_name("q")
+                search_bar.clear()
+                search_bar.send_keys(test_case)
+                search_bar.send_keys(Keys.RETURN)
+            driver.close()
+        except Exception as e:
+            print(e)
 
 
-with open("test_cases.txt", encoding='utf-8') as file:
-    lines = file.readlines()
-    test_cases = [line.rstrip() for line in lines if is_test_case(line)]
-    null_indices = [index for index, keyword in enumerate(test_cases) if keyword == "Null"]
-    test_cases = [test_case if test_case != "Null" else '' for test_case in test_cases]
+if __name__ == "__main__":
 
-
-# for test_case in test_cases:
-#     print(test_case)
-
-driver = webdriver.Chrome("./chromedriver.exe")
-driver.get("https://www.google.com")
-print(driver.title)
-for test_case in test_cases:
-    search_bar = driver.find_element_by_name("q")
-    search_bar.clear()
-    search_bar.send_keys(test_case)
-    search_bar.send_keys(Keys.RETURN)
-
-# print(driver.current_url)
-# driver.close()
+    google_test = SearchTest("test_cases.txt", "./chromedriver.exe", "https://www.google.com")
+    google_test.test()
