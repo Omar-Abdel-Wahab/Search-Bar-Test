4805fde4244348a6ed6f2f3e018b3433f74ff955
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2021-10-14 16:42:40 +0200

    Refactored variable and function names, applied "extract method" to some functions, and added new functions

--- search_test.py
+++ search_test.py
@@ -2,18 +2,45 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import logging
 
-TESTCASES_FILE_PATH = "testcases.txt"
-WEB_DRIVER_FILE_PATH = "chromedriver.exe"
+TESTCASES_FILE_PATH = "./testcases.txt"
+DRIVER_FILE_PATH = "./chromedriver.exe"
 SITE_URL = "https://www.google.com"
-QUERY_ELEMENT_NAME = "q"
-NULL_KEYWORD = "Null"
 
 
 class SearchTest:
     def __init__(self, file_path, driver_path, site):
-        self.file_path = file_path
-        self.driver_path = driver_path
-        self.site = site
+        self._file_path = file_path
+        self._driver_path = driver_path
+        self._site = site
+        self._driver = None
+        self._testcases = None
+        # self._current_testcase = None
+
+    def test_search_bar_error(self):
+        try:
+            self.test_search_bar()
+        except Exception as e:
+            logging.error(f'Error: {e}', exc_info=True)
+            quit()
+
+    def test_search_bar(self):
+        self.setup_driver()
+        self.extract_testcases()
+        self.get_site()
+        self.test_all_testcases()
+        self.close_driver()
+
+    def setup_driver(self):
+        self._driver = webdriver.Chrome(self._driver_path)
+
+    def extract_testcases(self):
+        null_keyword = "Null"
+        with open(self._file_path, encoding="utf-8") as file:
+            lines = file.readlines()
+            # Remove trailing whitespace from every testcase
+            testcases = [line.rstrip() for line in lines if self.is_testcase(line)]
+            # Replace "Null" keywords in the testcases.txt file with actual nulls
+            self._testcases = [testcase if testcase != null_keyword else "" for testcase in testcases]
 
     @staticmethod
     def is_testcase(line):
@@ -22,43 +49,28 @@ class SearchTest:
             return False
         return True
 
-    def driver_setup(self):
-        self.driver = webdriver.Chrome(self.driver_path)
-
     def get_site(self):
-        self.driver.get(self.site)
+        self._driver.get(self._site)
 
-    def extract_testcases(self):
-        with open(self.file_path, encoding="utf-8") as file:
-            lines = file.readlines()
-            # Remove trailing whitespace from every testcase
-            testcases = [line.rstrip() for line in lines if self.is_testcase(line)]
-            # Replace "Null" keywords in the testcases.txt file with actual nulls
-            self.testcases = [testcase if testcase != NULL_KEYWORD else "" for testcase in testcases]
+    def test_all_testcases(self):
+        for testcase in self._testcases:
+            self.test_single_testcase(testcase)
 
     def test_single_testcase(self, testcase):
-        search_bar = self.driver.find_element_by_name(QUERY_ELEMENT_NAME)
+        query_element_name = "q"
+        search_bar = self._driver.find_element_by_name(query_element_name)
         search_bar.clear()
         search_bar.send_keys(testcase)
         search_bar.send_keys(Keys.RETURN)
 
-    def driver_close(self):
-        self.driver.close()
+    def close_driver(self):
+        self._driver.close()
 
-    def test_search_bar(self):
-        try:
-            self.driver_setup()
-            self.extract_testcases()
-            self.get_site()
-            for testcase in self.testcases:
-                self.test_single_testcase(testcase)
-            self.driver_close()
-        except Exception as e:
-            logging.error(f'Error: {e}', exc_info=True)
-            quit()
+    def get_driver_current_url(self):
+        return self._driver.current_url
 
 
 if __name__ == "__main__":
 
-    google_bar = SearchTest(TESTCASES_FILE_PATH, WEB_DRIVER_FILE_PATH, SITE_URL)
-    google_bar.test_search_bar()
+    google_search_bar = SearchTest(TESTCASES_FILE_PATH, DRIVER_FILE_PATH, SITE_URL)
+    google_search_bar.test_search_bar()
--- test_automation.py
+++ test_automation.py
@@ -2,25 +2,24 @@ import unittest
 import urllib.parse
 from search_test import *
 
-SITE_RESULT_BASE_URL = "https://www.google.com/search?q="
-
 
 class ChromeSearch(unittest.TestCase):
 
     def setUp(self):
-        self.google_bar = SearchTest(TESTCASES_FILE_PATH, WEB_DRIVER_FILE_PATH, SITE_URL)
-        self.google_bar.driver_setup()
+        self.google_search_bar = SearchTest(TESTCASES_FILE_PATH, DRIVER_FILE_PATH, SITE_URL)
+        self.google_search_bar.setup_driver()
 
     def test_google_search_bar(self):
-        self.google_bar.extract_testcases()
-        self.google_bar.get_site()
-        for search_bar_testcase in self.google_bar.testcases:
-            self.google_bar.test_single_testcase(search_bar_testcase)
-            self.assertIn(f"{SITE_RESULT_BASE_URL}{urllib.parse.quote_plus(search_bar_testcase)}",
-                          self.google_bar.driver.current_url)
+        result_base_url = "https://www.google.com/search?q="
+        self.google_search_bar.extract_testcases()
+        self.google_search_bar.get_site()
+        for search_bar_testcase in self.google_search_bar._testcases:
+            self.google_search_bar.test_single_testcase(search_bar_testcase)
+            self.assertIn(f"{result_base_url}{urllib.parse.quote_plus(search_bar_testcase)}",
+                          self.google_search_bar.get_driver_current_url())
 
     def tearDown(self):
-        self.google_bar.driver.close()
+        self.google_search_bar.close_driver()
 
 
 if __name__ == "__main__":
