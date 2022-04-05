ff08738c1feab1959bd19bc709be7721164c2ac5
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-05 17:48:41 +0200

    Re-implemented test_search_bar functionality
    test_search_bar was a mess. It included dependencies, terribly slow and used GUI.
    Now, it is completely functional, cleaner and faster.
    Further commits will include all the required tests to test search_bar thoroughly

--- tests/test_search_bar.py
+++ tests/test_search_bar.py
@@ -1,26 +1,13 @@
 import unittest
-import urllib.parse
-from src.search_bar import SearchTester
-from constants.search_bar_constants import *
+from src.search_bar import search
 
 
-class ChromeSearch(unittest.TestCase):
-
-    def setUp(self):
-        self.google_search_bar = SearchTester(TESTCASES_FILE_PATH, DRIVER_FILE_PATH, SITE_URL)
-        self.google_search_bar.setup_driver()
+class SearchTest(unittest.TestCase):
 
     def test_google_search_bar(self):
-        result_base_url = "https://www.google.com/search?q="
-        self.google_search_bar.extract_testcases()
-        self.google_search_bar.get_site()
-        for search_bar_testcase in self.google_search_bar._testcases:
-            self.google_search_bar.test_single_testcase(search_bar_testcase)
-            self.assertIn(f"{result_base_url}{urllib.parse.quote_plus(search_bar_testcase)}",
-                          self.google_search_bar.get_driver_current_url())
-
-    def tearDown(self):
-        self.google_search_bar.close_driver()
+        testcase = "1"
+        result = search(testcase)
+        self.assertEqual(result.status_code, 200)
 
 
 if __name__ == "__main__":
