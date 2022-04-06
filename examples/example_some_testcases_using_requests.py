3ea5287f3ed384595cdc572da47326e2f3c8ee51
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-06 08:01:07 +0200

    Added another example to search multiple testcases

---
+++ examples/example_some_testcases_using_requests.py
@@ -0,0 +1,14 @@
+from src.search_bar import search
+
+
+def search_testcases(testcases):
+    for testcase in testcases:
+        search_result = search(testcase)
+        print(search_result.url)
+        print(search_result.ok)
+        print(search_result.status_code)
+
+
+if __name__ == "__main__":
+    testcases_to_search = ["1", "85", "word", "wordd", "Hi there!"]
+    search_testcases(testcases_to_search)
