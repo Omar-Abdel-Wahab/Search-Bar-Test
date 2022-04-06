a5a63f1a3d854a6015b580cf6d568cc85306a641
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-06 05:40:37 +0200

    Added a testcase in test_search_bar using patching
    Previous test_search_bar included interacting with external dependencies (Google search).
    Now, this is being mocked and performance is better of course after patching.

--- tests/test_search_bar.py
+++ tests/test_search_bar.py
@@ -1,13 +1,21 @@
 import unittest
+from unittest.mock import patch
+
+from constants.search_bar_constants import GOOGLE_SEARCH_URL
 from src.search_bar import search
 
 
 class SearchTest(unittest.TestCase):
 
-    def test_google_search_bar(self):
+    def test_positive_one_digit(self):
         testcase = "1"
-        result = search(testcase)
-        self.assertEqual(result.status_code, 200)
+        with patch("src.search_bar.get") as mocked_get:
+            mocked_get.return_value.status_code = 200
+
+            result = search(testcase)
+            mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
+
+            self.assertEqual(result.status_code, 200)
 
 
 if __name__ == "__main__":
