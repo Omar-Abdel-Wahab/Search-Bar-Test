436c2066847dcce8e03ddf9c9a9b6149935d9e5d
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-06 05:03:08 +0200

    Updated search_bar_constants
    search_bar_constants had unnecessary constants from now on, thus they were removed.
    search_bar_constants will now include search URLs basically

--- constants/search_bar_constants.py
+++ constants/search_bar_constants.py
@@ -1,6 +1,1 @@
-
-TESTCASES_FILE_PATH = "testcases.txt"
-DRIVER_FILE_PATH = "drivers/chrome/chromedriver.exe"
-SITE_URL = "https://www.google.com"
-
 GOOGLE_SEARCH_URL = "https://www.google.com/search?q="
