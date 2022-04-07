df2bc8fee29b814737ea3e1d85ba447023c150a8
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-07 13:27:04 +0200

    Modified abstract_browser
    Modified access modifiers to be non-private so that child classes can use it (access modifiers are not obligatory but for better readability)
    And adjusted setup_driver to take the driver_path needed to initialize the driver

--- src/abstract_browser.py
+++ src/abstract_browser.py
@@ -4,12 +4,12 @@ from selenium.webdriver.common.keys import Keys
 
 class Browser(ABC):
     def __init__(self, driver_path):
-        self._driver_path = driver_path
-        self._driver = None
-        self.setup_driver()
+        self.driver_path = driver_path
+        self.driver = None
+        self.setup_driver(driver_path)
 
     @abstractmethod
-    def setup_driver(self):
+    def setup_driver(self, driver_path):
         pass
 
     def search(self, url, testcase):
@@ -17,10 +17,10 @@ class Browser(ABC):
         self.submit_query(testcase)
 
     def load_search_page(self, url):
-        self._driver.get(url)
+        self.driver.get(url)
 
     def submit_query(self, testcase):
-        search_bar = self._driver.find_element_by_tag_name('input')
+        search_bar = self.driver.find_element_by_tag_name('input')
         search_bar.clear()
         search_bar.send_keys(testcase)
         search_bar.send_keys(Keys.RETURN)
