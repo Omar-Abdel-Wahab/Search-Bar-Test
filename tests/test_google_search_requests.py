f2cd86afef54620378c34f2ea35e16e3b1e77e07
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-07 05:51:50 +0200

    Refactored test_google_search_requests
    Previously, test_google_search_requests had duplicate code and it was longer.
    Now, it is more concise and there's no duplicate code.
    Further commits can have pytest instead of unittest to avoid boilerplate

--- tests/test_google_search_requests.py
+++ tests/test_google_search_requests.py
@@ -5,231 +5,96 @@ from constants.search_bar_constants import GOOGLE_SEARCH_URL
 from src.google_search_requests import search
 
 
-@patch("src.google_search_requests.get")
 class TestSearch(unittest.TestCase):
 
-    def test_positive_one_digit(self, mocked_get):
-        testcase = "1"
-        mocked_get.return_value.status_code = 200
+    @staticmethod
+    def perform_test(testcase):
+        with patch('src.google_search_requests.get') as mocked_get:
+            mocked_get.return_value.status_code = 200
 
-        response = search(testcase)
+            response = search(testcase)
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+            mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
+            assert response.status_code == 200
 
-    def test_positive_multi_digit(self, mocked_get):
-        testcase = "85"
-        mocked_get.return_value.status_code = 200
+    def test_positive_one_digit(self):
+        self.perform_test("1")
 
-        response = search(testcase)
+    def test_positive_multi_digit(self):
+        self.perform_test("85")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_negative_one_digit(self):
+        self.perform_test("-6")
 
-    def test_negative_one_digit(self, mocked_get):
-        testcase = "-6"
-        mocked_get.return_value.status_code = 200
+    def test_negative_multi_digit(self):
+        self.perform_test("-2749")
 
-        response = search(testcase)
+    def test_float(self):
+        self.perform_test("3.14159")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_zero(self):
+        self.perform_test("0")
 
-    def test_negative_multi_digit(self, mocked_get):
-        testcase = "-2749"
-        mocked_get.return_value.status_code = 200
+    def test_one_character(self):
+        self.perform_test("d")
 
-        response = search(testcase)
+    def test_small_case_word(self):
+        self.perform_test("experiment")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_capital_case_word(self):
+        self.perform_test("Experiment")
 
-    def test_float(self, mocked_get):
-        testcase = "3.14159"
-        mocked_get.return_value.status_code = 200
+    def test_upper_case_word(self):
+        self.perform_test("EXPERIMENT")
 
-        response = search(testcase)
+    def test_missing_letter_from_word(self):
+        self.perform_test("experimen")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_extra_letter_to_word(self):
+        self.perform_test("experimentt")
 
-    def test_zero(self, mocked_get):
-        testcase = "0"
-        mocked_get.return_value.status_code = 200
+    def test_different_language(self):
+        self.perform_test("تجربة")
 
-        response = search(testcase)
+    def test_sentence(self):
+        self.perform_test("I'm working from home")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_paragraph_word_limit(self):
+        self.perform_test("Lorem Ipsum simply dummy text printing typesetting industry Lorem Ipsum industry's standard "
+                          "dummy text ever since 1500s unknown printer took paper Contrary popular belief Lorem Ipsum "
+                          "simply random text piece classical Latin")
 
-    def test_one_character(self, mocked_get):
-        testcase = "d"
-        mocked_get.return_value.status_code = 200
+    def test_paragraph_exceeding_word_limit(self):
+        self.perform_test("There are many variations of passages of Lorem Ipsum available, but the majority have "
+                          "suffered alteration in some form, by injected humour, or randomised words which don't look "
+                          "even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be "
+                          "sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum "
+                          "generators on the Internet tend to repeat predefined chunks as necessary, making this the "
+                          "first true generator on the Internet ")
 
-        response = search(testcase)
+    def test_gibberish(self):
+        self.perform_test("asdfghjkl")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_query_operator(self):
+        self.perform_test("?q=")
 
-    def test_small_case_word(self, mocked_get):
-        testcase = "experiment"
-        mocked_get.return_value.status_code = 200
+    def test_ampersand(self):
+        self.perform_test("&")
 
-        response = search(testcase)
+    def test_word_operator(self):
+        self.perform_test("site:wikipedia.org")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_character_operator(self):
+        self.perform_test("@wikipedia")
 
-    def test_capital_case_word(self, mocked_get):
-        testcase = "Experiment"
-        mocked_get.return_value.status_code = 200
+    def test_url(self):
+        self.perform_test("https://www.google.com")
 
-        response = search(testcase)
+    def test_null(self):
+        self.perform_test("")
 
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_upper_case_word(self, mocked_get):
-        testcase = "EXPERIMENT"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_missing_letter_from_word(self, mocked_get):
-        testcase = "experimen"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_extra_letter_to_word(self, mocked_get):
-        testcase = "experimentt"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_different_language(self, mocked_get):
-        testcase = "تجربة"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_sentence(self, mocked_get):
-        testcase = "I'm working from home"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_paragraph_word_limit(self, mocked_get):
-        testcase = "Lorem Ipsum simply dummy text printing typesetting industry Lorem Ipsum industry's standard dummy" \
-                   "text ever since 1500s unknown printer took paper Contrary popular belief Lorem Ipsum simply " \
-                   "random text piece classical Latin text"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_paragraph_exceeding_word_limit(self, mocked_get):
-        testcase = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered " \
-                   "alteration in some form, by injected humour, or randomised words which don't look even slightly " \
-                   "believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't " \
-                   "anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the " \
-                   "Internet tend to repeat predefined chunks as necessary, making this the first true generator on " \
-                   "the Internet "
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_gibberish(self, mocked_get):
-        testcase = "asdfghjkl"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_query_operator(self, mocked_get):
-        testcase = "?q="
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_ampersand(self, mocked_get):
-        testcase = "&"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_word_operator(self, mocked_get):
-        testcase = "site:wikipedia.org"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_character_operator(self, mocked_get):
-        testcase = "@wikipedia"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url(self, mocked_get):
-        testcase = "https://www.google.com"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_null(self, mocked_get):
-        testcase = ""
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_encoding(self, mocked_get):
-        testcase = "https%3A%2F%2Fwww.google.com"
-        mocked_get.return_value.status_code = 200
-
-        response = search(testcase)
-
-        mocked_get.assert_called_with(f"{GOOGLE_SEARCH_URL}{testcase}")
-        self.assertEqual(response.status_code, 200)
+    def test_url_encoding(self):
+        self.perform_test("https%3A%2F%2Fwww.google.com")
 
 
 if __name__ == "__main__":
