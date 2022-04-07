e4706b966c456d266b3a4022657bea643e5a7d2c
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-04-07 13:45:07 +0200

    Omitted potential bug in abstract_browser
    Previously, every time a search happens, the page would be loaded, thus I deleted previous 'search' method
    Also, a 'close' method has been added to close the browser when finished

--- src/abstract_browser.py
+++ src/abstract_browser.py
@@ -12,15 +12,14 @@ class Browser(ABC):
     def setup_driver(self, driver_path):
         pass
 
-    def search(self, url, testcase):
-        self.load_search_page(url)
-        self.submit_query(testcase)
-
     def load_search_page(self, url):
         self.driver.get(url)
 
-    def submit_query(self, testcase):
+    def search(self, testcase):
         search_bar = self.driver.find_element_by_tag_name('input')
         search_bar.clear()
         search_bar.send_keys(testcase)
         search_bar.send_keys(Keys.RETURN)
+
+    def close(self):
+        self.driver.close()
